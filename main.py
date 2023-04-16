59088bc11e83bdd721a6092d903baef6748febfd
Peter Tumali <ptumali@uci.edu>
2023-04-15 21:13:47 -0700

    Visualization for line chart
    Make a function that gets the visualization for the line chart that shows the amount of crime records (2015-2022)

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import sqlite3
 import plotly.express as px
 from pathlib import Path
 
-# TODO Line chart showing years and crime amount
-# TODO Pie chart proportions of crimes (types)
 # TODO Segmentation bar graph of crimes based on precincts
 # TODO plotly map
 # TODO Bar graph(x-axis=day of time, y-axis=amount of crimes)
@@ -11,7 +9,17 @@ from pathlib import Path
 connection = sqlite3.connect(Path("D:/UCI/Montreal-Crime-Analysis-Datathon-2023-/crime.db"))
 cursor = connection.cursor()
 
-def find_category_amount(categ_name: str) -> int:
+def organize_category_amount() -> dict:
+    categ_dict = dict()
+    categ_dict['Breaking and Entering'] = _find_category_amount('Introduction')
+    categ_dict['Theft from/to a motor vehicle'] = _find_category_amount('Vol dans / sur véhicule à moteur')
+    categ_dict['Theft of a motor vehicle'] = _find_category_amount('Vol de véhicule à moteur')
+    categ_dict['Mischief'] = _find_category_amount('Méfait')
+    categ_dict['Robbery'] = _find_category_amount('Vols qualifiés')
+    categ_dict['Murder resulting death'] = _find_category_amount('Infractions entrainant la mort')
+    return categ_dict
+
+def _find_category_amount(categ_name: str) -> int:
     cursor.execute('SELECT categorie ,count(*) '
                     'FROM crime '
                     'WHERE categorie =? '
@@ -21,21 +29,29 @@ def find_category_amount(categ_name: str) -> int:
     result = cursor.fetchall()
     return result[0][1]
 
-def organize_category_amount() -> dict:
-    categ_dict = dict()
-    categ_dict['Introduction'] = find_category_amount('Introduction')
-    categ_dict['Vol dans / sur véhicule à moteur'] = find_category_amount('Vol dans / sur véhicule à moteur')
-    categ_dict['Vol de véhicule à moteur'] = find_category_amount('Vol de véhicule à moteur')
-    categ_dict['Méfait'] = find_category_amount('Méfait')
-    categ_dict['Vols qualifiés'] = find_category_amount('Vols qualifiés')
-    categ_dict['Infractions entrainant la mort'] = find_category_amount('Infractions entrainant la mort')
-    return categ_dict
-
 def pie_chart_crime_prop() -> None:
     crime_dict = organize_category_amount()
     fig = px.pie(values=crime_dict.values(), names=crime_dict.keys())
     fig.show()
 
+def line_graph_years_and_crime() -> None:
+    year_crime_amount_dict = _find_year_crime_amount()
+    fig = px.line(x= year_crime_amount_dict.keys(),
+                  y= year_crime_amount_dict.values(),
+                  title = 'Amount of crime in Montreal over time (2015-2022)')
+    fig.show()
+def _find_year_crime_amount() -> dict:
+    year_crime_amount_dict = dict()
+    cursor.execute("SELECT SUBSTR(DATE, 1, 4) AS year, COUNT(*) AS count "
+                   "FROM crime "
+                   "WHERE SUBSTR(DATE, 1, 4) <> '2023' "
+                   "GROUP BY SUBSTR(DATE, 1, 4);")
+    result = cursor.fetchall()
+    for i in result:
+        year_crime_amount_dict[i[0]] = i[1]
+    return year_crime_amount_dict
+
+
 
 if __name__ == "__main__":
-    pie_chart_crime_prop()
+    line_graph_years_and_crime()
